d225e4e00082628ef4978ea0961f8dbe514c540a
Ashish <ashishkap157@gmail.com>
2025-04-27 15:29:34 +0530

    queue

--- services/queue.js
+++ services/queue.js
@@ -5,9 +5,9 @@ const scraperQueue = new Queue("scraper", {
   // redis: { host: "localhost", port: 6379 },
   redis: {
     url: process.env.REDIS_URL,
-    // tls: {
-    //   rejectUnauthorized: false, // For Railway's Redis proxy
-    // },
+    tls: {
+      rejectUnauthorized: false, // For Railway's Redis proxy
+    },
   },
   settings: {
     stalledInterval: 300000, // 5 minutes
@@ -18,7 +18,7 @@ const scraperQueue = new Queue("scraper", {
 });
 
 // Process with 3 concurrent workers
-scraperQueue.process(3, scrapeJob);
+scraperQueue.process(1, scrapeJob);
 
 export default scraperQueue;
 
