5b8b3cd7f1191648dfa7ae558784f99446ad8895
Ashish <ashishkap157@gmail.com>
2025-04-27 21:56:58 +0530

    reddis url

--- services/queue.js
+++ services/queue.js
@@ -3,20 +3,21 @@ import scrapeJob from "../jobs/scrapeJob.js";
 
 const scraperQueue = new Queue("scraper", {
   // redis: { host: "localhost", port: 6379 },
-  // redis: {
-  //   url: process.env.REDIS_URL,
-  //   tls: {
-  //     rejectUnauthorized: false,
-  //   },
-  // },
   redis: {
-    // host: process.env.REDIS_HOST || "redis.railway.internal",
-    // port: parseInt(process.env.REDIS_PORT) || 6379,
-    // password: process.env.REDIS_PASSWORD || "sOIBUPgdiXNvtYknQhWudMAQCFZwlvsA",
-    host: "redis.railway.internal",
-    port: 6379,
-    password: "sOIBUPgdiXNvtYknQhWudMAQCFZwlvsA",
+    // url: process.env.REDIS_URL,
+    url:"redis-cli -u redis://default:sOIBUPgdiXNvtYknQhWudMAQCFZwlvsA@interchange.proxy.rlwy.net:57649",
+    tls: {
+      rejectUnauthorized: false,
+    },
   },
+  // redis: {
+  //   // host: process.env.REDIS_HOST ,
+  //   // port: parseInt(process.env.REDIS_PORT) ,
+  //   // password: process.env.REDIS_PASSWORD,
+  //   host: "redis.railway.internal",
+  //   port: 6379,
+  //   password: "sOIBUPgdiXNvtYknQhWudMAQCFZwlvsA",
+  // },
   settings: {
     stalledInterval: 300000, // 5 minutes
     maxStalledCount: 2,
