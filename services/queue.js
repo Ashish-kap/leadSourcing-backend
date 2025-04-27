10a4874cb3599fe24894cdeb7dade56c4ec929de
Ashish <ashishkap157@gmail.com>
2025-04-27 22:47:58 +0530

    reddis url v3

--- services/queue.js
+++ services/queue.js
@@ -4,8 +4,7 @@ import scrapeJob from "../jobs/scrapeJob.js";
 const scraperQueue = new Queue("scraper", {
   // redis: { host: "localhost", port: 6379 },
   redis: {
-    // url: process.env.REDIS_URL,
-    url: "redis://default:sOIBUPgdiXNvtYknQhWudMAQCFZwlvsA@interchange.proxy.rlwy.net:57649",
+    url: process.env.REDIS_URL,
     tls: {
       rejectUnauthorized: false,
     },
@@ -14,9 +13,6 @@ const scraperQueue = new Queue("scraper", {
   //   // host: process.env.REDIS_HOST ,
   //   // port: parseInt(process.env.REDIS_PORT) ,
   //   // password: process.env.REDIS_PASSWORD,
-  //   host: "redis.railway.internal",
-  //   port: 6379,
-  //   password: "sOIBUPgdiXNvtYknQhWudMAQCFZwlvsA",
   // },
   settings: {
     stalledInterval: 300000, // 5 minutes
