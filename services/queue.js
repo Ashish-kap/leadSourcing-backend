a5193bef45934569cefb5b18f2d3f2fbcdc1a8d9
Ashish <ashishkap157@gmail.com>
2025-05-04 19:02:50 +0530

    scrapper queue

--- services/queue.js
+++ services/queue.js
@@ -3,10 +3,6 @@ import scrapeJob from "../jobs/scrapeJob.js";
 
 const scraperQueue = new Queue("scraper", {
   // redis: { host: "localhost", port: 6379 },
-  // redis: {
-  //   url: process.env.REDIS_URL,
-  //   tls: { rejectUnauthorized: false },
-  // },
   redis: process.env.REDIS_URL,
   settings: {
     stalledInterval: 300000, // 5 minutes
@@ -25,6 +21,6 @@ scraperQueue.on("connected", () => {
 });
 
 // Process with 3 concurrent workers
-scraperQueue.process(3, scrapeJob);
+scraperQueue.process(1, scrapeJob);
 
 export default scraperQueue;
