a3fee63ce2ea3507d0f1ecb99d86c1f9f0d1de89
Ashish <ashishkap157@gmail.com>
2025-07-28 22:20:03 +0530

    redis url

--- server.js
+++ server.js
@@ -35,12 +35,6 @@ app.use("/admin", serverAdapter.getRouter());
 app.use("/api", scraperRouter);
 
 
-// // // // Static files for frontend
-// app.use(express.static(path.join(__dirname, "dist")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "dist", "index.html"));
-// });
-
 
 // Start Server
 const PORT = process.env.PORT || 3000;
