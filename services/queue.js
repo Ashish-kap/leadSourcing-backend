042d0252659cab1a547e0f829e35e0a727f79de7
Ashish <ashishkap157@gmail.com>
2025-07-28 22:20:03 +0530

    redis url

--- services/queue.js
+++ services/queue.js
@@ -2,11 +2,19 @@ import dotenv from "dotenv";
 dotenv.config();
 import Queue from "bull";
 import scrapeJob from "../jobs/scrapeJob.js";
+import { URL } from "url";
 
+
+console.log("redisUrl",redisUrl)
+
+const redisUrl = new URL(process.env.REDIS_HOST || "redis://localhost:6379");
 const scraperQueue = new Queue("scraper", {
   redis: {
-    host: process.env.REDIS_HOST || "localhost",
-    port: 6379,
+    // host: process.env.REDIS_URL || "localhost",
+    // port: 6379,
+    host: redisUrl.hostname,
+    port: redisUrl.port,
+    password: redisUrl.password.replace("default:", ""), 
   },
   settings: {
     stalledInterval: 300000, // 5 minutes
